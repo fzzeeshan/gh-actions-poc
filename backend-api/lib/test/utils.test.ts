26b64ee8377d426c74bb98382b81b0035e0dca07
fzzeeshan <zeeshancodez@gmail.com>
2023-07-01 00:21:42 +0100

    adding basic services

---
+++ backend-api/lib/test/utils.test.ts
@@ -0,0 +1,14 @@
+import { sayHello } from '../src/utils'; // Assuming the code is in a separate module file
+
+describe('sayHello', () => {
+  it('should return a greeting message with the provided name', () => {
+    const name = 'John';
+    const result = sayHello(name);
+    expect(result).toBe('Hello John');
+  });
+
+  it('should return a greeting message with an empty string if no name is provided', () => {
+    const result = sayHello('');
+    expect(result).toBe('Hello ');
+  });
+});
--- src/microservice-two/function.ts
+++
@@ -1,7 +0,0 @@
-import { sayHello } from "../lib/utils";
-
-var welcomeMessage : string;
-
-welcomeMessage = sayHello('Shave');
-
-console.log(welcomeMessage);
