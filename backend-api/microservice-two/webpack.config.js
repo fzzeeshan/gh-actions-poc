6727c05af407a5f0e75dd0ca44fe6959dcc5a990
fzzeeshan <zeeshancodez@gmail.com>
2023-07-01 22:25:09 +0100

    adding webpack as build tool

---
+++ backend-api/microservice-two/webpack.config.js
@@ -0,0 +1,33 @@
+const { resolve } = require("path");
+const webpack = require("webpack");
+const packageJson = require("./package.json");
+
+module.exports = {
+  entry: "./src/program.ts",
+  output: {
+    filename: `bundle.${packageJson.version}.js`,
+    path: resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_module/,
+        use: "ts-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": {
+        VERSION: JSON.stringify(packageJson.version),
+      },
+    }),
+    // ... your other plugins
+  ],
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  target: "node",
+  mode: "production",
+};
